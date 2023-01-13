63b7b89a3da59f3e7e055cad5f7a9c96b637c9ac
Ryan Baer <ryanjbaer@outlook.com>
2023-01-13 12:13:04 -0700

    add method for filtering trips for user

--- src/User.js
+++ src/User.js
@@ -3,9 +3,16 @@ class User {
         this.id = newUser.id
         this.name = newUser.name
         this.travelerType = newUser.travelerType
+        this.userTrips = []
+        //pass in all of this specific users trips from the global variable theyre stored in
     }
     returnFirstName(){
         return this.name.split(' ')[0]
+    }
+    filterTrips(allTrips){
+        this.userTrips = allTrips.filter(trip => {
+            return trip.userID === this.id
+        })
     }
 }
 //should have a method that finds this users past, pending, and upcoming trips
