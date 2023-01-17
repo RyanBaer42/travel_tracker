c0dd8d881195c0f8563842195fcc8bd56aa047d2
Ryan Baer <ryanjbaer@outlook.com>
2023-01-17 09:46:22 -0700

    add file and export all api calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,13 +1,23 @@
+const errorMessage = document.getElementById('errorMessage')
+const topHeader = document.getElementById('topHeader')
+const tripsSection = document.getElementById('tripsSection')
+
 function loadGetData(link){
     return fetch(link)
     .then((response) => {
         if (response.ok){
                 return response.json()
         }
+        throw Promise.reject(response)
     })
     .then((data) => {
+        errorHandling('message', 'noError')
         return data
     })
+    .catch((response) => {
+        errorHandling('Sorry, the server is down. Try again later', 'error')
+        console.log('Something went wrong: ', response);
+    });
 }
 
 function postNewTrip(newTrip){
@@ -18,6 +28,15 @@ function postNewTrip(newTrip){
             'Content-Type': 'application/json'
         }
       })
+}
+
+function errorHandling(message, display) {
+    if (display === 'error') {
+        errorMessage.innerText = message
+        errorMessage.classList.remove('hidden')
+        topHeader.classList.add('hidden')
+        tripsSection.classList.add('hidden')
+    } 
 }
 
 export default { loadGetData, postNewTrip};
